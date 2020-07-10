3b6619b9ecab5c50fe493a737e221888599c263e
notomo <notomo.motono@gmail.com>
2020-07-10 19:29:52 +0900

    Use nvim_list_runtime_paths

--- lua/thetto/source/runtimepath.lua
+++ lua/thetto/source/runtimepath.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.make = function()
   local candidates = {}
-  local paths = vim.split(vim.o.runtimepath, ",")
+  local paths = vim.api.nvim_list_runtime_paths()
   for _, path in ipairs(paths) do
     table.insert(candidates, {value = path, path = path})
   end
