6ee17e202f6938c35f7e584e1c38f3caff150725
notomo <notomo.motono@gmail.com>
2020-07-12 20:48:22 +0900

    Refactor command parse phase

--- lua/thetto/command.lua
+++ lua/thetto/command.lua
@@ -1,71 +1,69 @@
 local thetto = require "thetto/thetto"
+local util = require "thetto/util"
 
 local M = {}
 
-M.parse_open_args = function(raw_args)
-  local args = {
-    insert = true,
-    resume = false
-  }
+M.parse_args = function(raw_args, value_name, default)
+  local args = vim.deepcopy(default)
 
   for _, a in ipairs(raw_args) do
     if not vim.startswith(a, "--") then
-      args.source_name = a
+      args[value_name] = a
     elseif vim.startswith(a, "--no-") then
       local key = a:sub(#("--no-") + 1)
       args[key] = false
     elseif vim.startswith(a, "--") and not a:find("=") then
       local key = a:sub(#("--") + 1)
       args[key] = true
+    else
+      return nil, "could not parse arg: " .. a
     end
-  end
-
-  if args.source_name == nil and not args.resume then
-    return nil, "no source"
   end
 
   return args, nil
 end
 
 M.open = function(...)
-  local args, parse_err = M.parse_open_args({...})
+  local args, parse_err =
+    M.parse_args(
+    {...},
+    "source_name",
+    {
+      insert = true,
+      resume = false
+    }
+  )
   if parse_err ~= nil then
-    return vim.api.nvim_err_write(parse_err .. "\n")
+    return util.print_err(parse_err)
+  end
+
+  if args.source_name == nil and not args.resume then
+    return util.print_err("no source")
   end
 
   local err = thetto.start(args)
   if err ~= nil then
-    return vim.api.nvim_err_write(err .. "\n")
+    return util.print_err(err)
   end
-end
-
-M.parse_execute_args = function(raw_args)
-  local args = {
-    action = "default",
-    quit = true
-  }
-
-  for _, a in ipairs(raw_args) do
-    if not vim.startswith(a, "--") then
-      args.action = a
-    elseif vim.startswith(a, "--no-") then
-      local key = a:sub(#("--no-") + 1)
-      args[key] = false
-    end
-  end
-
-  return args, nil
 end
 
 M.execute = function(...)
-  local args, parse_err = M.parse_execute_args({...})
+  local args, parse_err =
+    M.parse_args(
+    {...},
+    "action",
+    {
+      action = "default",
+      quit = true
+    }
+  )
   if parse_err ~= nil then
-    return vim.api.nvim_err_write(parse_err .. "\n")
+    return util.print_err(parse_err)
   end
 
   local err = thetto.execute(args)
   if err ~= nil then
-    return vim.api.nvim_err_write(err .. "\n")
+    return util.print_err(err)
   end
 end
 
