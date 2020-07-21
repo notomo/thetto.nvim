9f39ce238b2bfa0c8050ea8a6ee7a0a43d195d47
notomo <notomo.motono@gmail.com>
2020-07-21 11:42:08 +0900

    Add vim/filetype source

---
+++ lua/thetto/source/vim/filetype.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.make = function()
+  local items = {}
+  local names = vim.fn.getcompletion("*", "filetype")
+  for _, name in ipairs(names) do
+    table.insert(items, {value = name})
+  end
+  return items
+end
+
+M.kind_name = "word"
+
+return M
---
+++ test/lua/thetto/source/vim/filetype_spec.lua
@@ -0,0 +1,18 @@
+local helper = require "test.helper"
+local assert = helper.assert
+local command = helper.command
+
+describe("vim/filetype source", function()
+
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
+  it("can show filetypes", function()
+    command("Thetto vim/filetype")
+    helper.sync_input({"lua"})
+
+    command("ThettoDo move_to_list")
+    assert.current_line("lua")
+  end)
+
+end)
