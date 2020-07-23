fa8fefd230ebaff8101585f8a323645c2da1dc4f
notomo <notomo.motono@gmail.com>
2020-07-23 10:23:27 +0900

    Add current branch highlight

--- lua/thetto/source/git/branch.lua
+++ lua/thetto/source/git/branch.lua
@@ -6,11 +6,22 @@ M.collect = function(self, opts)
     table.insert(cmd, "--all")
   end
 
+  local current_branch = nil
+  local get_current_job = self.jobs.new({"git", "rev-parse", "--abbrev-ref", "HEAD"}, {
+    on_exit = function(job_self)
+      current_branch = job_self:get_stdout()[1]
+    end,
+    cwd = opts.cwd,
+  })
+  get_current_job:start()
+  get_current_job:wait(1000)
+
   local job = self.jobs.new(cmd, {
     on_exit = function(job_self)
       local items = {}
       for _, output in ipairs(job_self:get_stdout()) do
-        table.insert(items, {value = output})
+        local is_current_branch = output == current_branch
+        table.insert(items, {value = output, is_current_branch = is_current_branch})
       end
       self.set(items)
     end,
@@ -18,6 +29,17 @@ M.collect = function(self, opts)
   })
 
   return {}, job
+end
+
+M.highlight = function(self, bufnr, items)
+  local ns = self.highlights.reset()
+  for i, item in ipairs(items) do
+    if not item.is_current_branch then
+      goto continue
+    end
+    vim.api.nvim_buf_add_highlight(bufnr, ns, "Type", i - 1, 0, -1)
+    ::continue::
+  end
 end
 
 M.kind_name = "git/branch"
