e0cfb6c7bc69b969d3aa78fea83d53a035014e8e
notomo <notomo.motono@gmail.com>
2020-07-31 22:54:04 +0900

    Improve line kind detection

--- lua/thetto/source/line.lua
+++ lua/thetto/source/line.lua
@@ -1,10 +1,19 @@
 local M = {}
 
 M.collect = function()
+  local bufnr = 0
+  local kind_name = nil
+  local path = vim.api.nvim_buf_get_name(bufnr)
+  if not vim.bo.modified and vim.fn.filereadable(path) == 1 then
+    kind_name = "file"
+  else
+    path = nil
+  end
+
   local items = {}
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
   for i, line in ipairs(lines) do
-    table.insert(items, {value = line, row = i})
+    table.insert(items, {value = line, row = i, kind_name = kind_name, path = path})
   end
   return items
 end
