ef734a85282aec0aac822cba54689ef84a5fc15a
notomo <notomo.motono@gmail.com>
2020-09-30 21:12:24 +0900

    Fix keymap open validation

--- lua/thetto/kind/vim/keymap.lua
+++ lua/thetto/kind/vim/keymap.lua
@@ -12,14 +12,14 @@ M.action_execute = function(_, items)
   end
 end
 
-local to_files = function(items)
+local to_files = function(self, items)
   local files = {}
   for _, item in ipairs(items) do
     local result = vim.api.nvim_exec("verbose map " .. item.keymap.lhs, true)
     local outputs = vim.split(result, "\n", true)
     for _, output in ipairs(outputs) do
       local path = output:match("^%s+Last%s+set%s+from%s+(%S+)")
-      if path ~= nil then
+      if path ~= nil and self.filelib.readable(vim.fn.expand(path)) then
         table.insert(files, {path = path, row = item.keymap.row})
         break
       end
@@ -31,15 +31,15 @@ end
 local file_kind = require("thetto/kind/file")
 
 M.action_open = function(self, items)
-  file_kind.action_open(self, to_files(items))
+  file_kind.action_open(self, to_files(self, items))
 end
 
 M.action_tab_open = function(self, items)
-  file_kind.action_tab_open(self, to_files(items))
+  file_kind.action_tab_open(self, to_files(self, items))
 end
 
 M.action_vsplit_open = function(self, items)
-  file_kind.action_vsplit_open(self, to_files(items))
+  file_kind.action_vsplit_open(self, to_files(self, items))
 end
 
 M.default_action = "execute"
