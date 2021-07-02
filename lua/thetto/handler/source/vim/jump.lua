76abb5d3fff12d0acba561e0868d2da2e4eeb77b
notomo <notomo.motono@gmail.com>
2021-07-02 20:40:25 +0900

    Add vim/jump source per_file option

--- lua/thetto/handler/source/vim/jump.lua
+++ lua/thetto/handler/source/vim/jump.lua
@@ -1,13 +1,19 @@
 local M = {}
 
+M.opts = {per_file = false}
+
 function M.collect(self, opts)
   local items = {}
 
   local jumps = vim.fn.reverse(vim.fn.getjumplist(0)[1])
   local home = self.pathlib.home()
   local to_relative = self.pathlib.relative_modifier(opts.cwd)
+  local current_path = vim.api.nvim_buf_get_name(0)
   for _, jump in ipairs(jumps) do
     local bufnr = jump.bufnr
+    if bufnr == 0 then
+      bufnr = vim.api.nvim_get_current_buf()
+    end
     if not vim.api.nvim_buf_is_valid(bufnr) then
       goto continue
     end
@@ -15,8 +21,13 @@ function M.collect(self, opts)
       vim.fn.bufload(bufnr)
     end
 
-    local line = vim.api.nvim_buf_get_lines(bufnr, jump.lnum - 1, jump.lnum, false)[1] or ""
     local path = vim.uri_to_fname(vim.uri_from_bufnr(bufnr))
+    if self.opts.per_file and (path == current_path or path == "") then
+      goto continue
+    end
+    current_path = path
+
+    local line = vim.api.nvim_buf_get_lines(bufnr, jump.lnum - 1, jump.lnum, false)[1] or ""
     local name = to_relative(path)
     local row = jump.lnum
     local label = ("%s:%d"):format(name:gsub(home, "~"), row)
@@ -27,7 +38,7 @@ function M.collect(self, opts)
       value = line,
       path = path,
       row = row,
-      bufnr = jump.bufnr,
+      bufnr = bufnr,
       column = jump.col,
       column_offsets = {["path:relative"] = 0, value = #label + 1},
     })
