fef4836a03afcb025ae49f6d834d41de87e5d53b
notomo <notomo.motono@gmail.com>
2021-07-15 11:35:42 +0900

    Add zsh history source

---
+++ lua/thetto/handler/kind/cmd/shell/cmd.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+function M.action_execute(_, items)
+  for _, item in ipairs(items) do
+    vim.cmd("tabedit")
+    vim.fn.termopen({item.shell}, {cwd = item.cwd})
+    vim.fn.chansend(vim.bo.channel, item.value .. "\nexit\n")
+  end
+  if items[1] then
+    vim.cmd("startinsert!")
+  end
+end
+
+function M.action_tab_open(_, items)
+  for _, item in ipairs(items) do
+    vim.cmd("tabedit")
+    vim.fn.termopen({item.shell}, {cwd = item.cwd})
+    vim.fn.chansend(vim.bo.channel, item.value .. "\n")
+  end
+  if items[1] then
+    vim.cmd("startinsert!")
+  end
+end
+
+M.default_action = "execute"
+
+return M
