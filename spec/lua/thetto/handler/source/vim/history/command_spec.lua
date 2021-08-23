c7326148eec149d346e0f8665cf8d7d45e69e58b
notomo <notomo.motono@gmail.com>
2021-08-24 08:50:28 +0900

    Add vim/history/command source

---
+++ spec/lua/thetto/handler/source/vim/history/command_spec.lua
@@ -0,0 +1,17 @@
+local helper = require("thetto.lib.testlib.helper")
+local thetto = helper.require("thetto")
+
+describe("vim/history/command source", function()
+
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
+  it("can show command history", function()
+    vim.fn.histadd("cmd", "8888")
+
+    thetto.start("vim/history/command", {opts = {insert = false}})
+
+    assert.exists_pattern("8888")
+  end)
+
+end)
