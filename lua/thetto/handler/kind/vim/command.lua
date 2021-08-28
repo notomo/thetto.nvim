52bb23b392cd6c0fa666f9573832d54a095e6bb6
notomo <notomo.motono@gmail.com>
2021-08-28 14:34:28 +0900

    Fix vim/command's action on error

--- lua/thetto/handler/kind/vim/command.lua
+++ lua/thetto/handler/kind/vim/command.lua
@@ -2,7 +2,10 @@ local M = {}
 
 function M.action_execute(_, items)
   for _, item in ipairs(items) do
-    vim.cmd(item.value)
+    local ok, err = pcall(vim.cmd, item.value)
+    if not ok then
+      return nil, err
+    end
   end
 end
 
