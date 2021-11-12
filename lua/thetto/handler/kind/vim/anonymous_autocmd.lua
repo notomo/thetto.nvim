4c002898ffeb7cbefa2cd473da8498bf7fc5fc58
notomo <notomo.motono@gmail.com>
2021-11-12 09:54:24 +0900

    Add autocmd delete action

--- lua/thetto/handler/kind/vim/anonymous_autocmd.lua
+++ lua/thetto/handler/kind/vim/anonymous_autocmd.lua
@@ -6,4 +6,16 @@ function M.action_delete_group(_, items)
   end
 end
 
+function M.action_delete_autocmd_by_pattern(_, items)
+  for _, item in ipairs(items) do
+    vim.cmd(("autocmd! %s %s %s"):format(item.autocmd.group, item.autocmd.event, item.autocmd.pattern))
+  end
+end
+
+function M.action_delete_autocmd_by_event(_, items)
+  for _, item in ipairs(items) do
+    vim.cmd(("autocmd! %s %s"):format(item.autocmd.group, item.autocmd.event))
+  end
+end
+
 return M
--- spec/lua/thetto/handler/source/vim/autocmd_spec.lua
+++ spec/lua/thetto/handler/source/vim/autocmd_spec.lua
@@ -18,7 +18,7 @@ describe("vim/autocmd source", function()
     assert.exists_pattern("hoge_autocmd")
   end)
 
-  it("can delete autocmd", function()
+  it("can delete autocmd group", function()
     vim.cmd("augroup target_group")
     vim.cmd("autocmd VimResume <buffer> echomsg 'target_autocmd'")
     vim.cmd("augroup END")
@@ -36,6 +36,28 @@ describe("vim/autocmd source", function()
     thetto.execute("move_to_list")
 
     assert.no.exists_pattern("target_autocmd")
+  end)
+
+  it("can delete an autocmd by pattern", function()
+    vim.cmd("augroup target_group")
+    vim.cmd("autocmd VimResume test1 echomsg 'the_target_autocmd'")
+    vim.cmd("autocmd VimResume test2 echomsg 'not_target_autocmd'")
+    vim.cmd("augroup END")
+
+    thetto.start("vim/autocmd")
+    helper.sync_input({"target_autocmd"})
+
+    thetto.execute("move_to_list")
+    helper.search("target_autocmd")
+
+    thetto.execute("delete_autocmd_by_pattern")
+
+    thetto.start("vim/autocmd")
+    helper.sync_input({"target_autocmd"})
+    thetto.execute("move_to_list")
+
+    assert.no.exists_pattern("the_target_autocmd")
+    assert.exists_pattern("not_target_autocmd")
   end)
 
 end)
