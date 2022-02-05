226d233dadd0fca026ad616839cd6ddff37749e7
notomo <notomo.motono@gmail.com>
2022-02-05 17:42:02 +0900

    Fix help open action

--- lua/thetto/handler/kind/vim/help.lua
+++ lua/thetto/handler/kind/vim/help.lua
@@ -1,32 +1,32 @@
 local M = {}
 
-function M.action_open(_, items)
+function M.action_open(self, items)
   for _, item in ipairs(items) do
-    M._open(item, "", "action_open")
+    self:_open(item, "", "action_open")
     vim.cmd("only")
   end
 end
 
-function M.action_tab_open(_, items)
+function M.action_tab_open(self, items)
   for _, item in ipairs(items) do
-    M._open(item, "tab", "action_tab_open")
+    self:_open(item, "tab", "action_tab_open")
   end
 end
 
-function M.action_vsplit_open(_, items)
+function M.action_vsplit_open(self, items)
   for _, item in ipairs(items) do
-    M._open(item, "vertical", "action_vsplit_open")
+    self:_open(item, "vertical", "action_vsplit_open")
   end
 end
 
 local file_kind = require("thetto.handler.kind.file")
 
-function M._open(item, help_prefix, edit_action)
+function M._open(self, item, help_prefix, edit_action)
   local ok = pcall(vim.cmd, ("%s help %s"):format(help_prefix, item.value))
   if ok then
     return
   end
-  file_kind[edit_action](file_kind, { item })
+  file_kind[edit_action](self, { item })
   vim.cmd([[nohlsearch]])
   vim.bo.buftype = "help"
   vim.bo.modifiable = false
