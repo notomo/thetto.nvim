ba4b8f113583e7d836462b05ff23ad0e54c498a3
notomo <notomo.motono@gmail.com>
2022-03-04 21:32:12 +0900

    Add url/bookmark

---
+++ lua/thetto/handler/source/url/bookmark.lua
@@ -0,0 +1,44 @@
+local pathlib = require("thetto.lib.path")
+local filelib = require("thetto.lib.file")
+
+local M = {}
+
+local char_to_hex = function(c)
+  return ("%%%02X"):format(c:byte())
+end
+
+local encode = function(url)
+  url = url:gsub("\n", "\r\n")
+  url = url:gsub("([^%w _%%%-%.~/:%?@=&#])", char_to_hex)
+  url = url:gsub(" ", "+")
+  return url
+end
+
+M.opts = { file_path = nil, default_lines = {} }
+
+function M.collect(self)
+  local file_path = self.opts.file_path or pathlib.user_data_path("url_bookmark.txt")
+  if filelib.create_if_need(file_path) then
+    local f = io.open(file_path, "w")
+    for _, line in ipairs(self.opts.default_lines) do
+      f:write(line .. "\n")
+    end
+    f:close()
+  end
+
+  local items = {}
+  local f = io.open(file_path, "r")
+  local i = 1
+  for line in f:lines() do
+    local url = encode(vim.fn.reverse(vim.split(line, "\t", true))[1])
+    table.insert(items, { value = line, path = file_path, row = i, url = url })
+    i = i + 1
+  end
+  f:close()
+
+  return items
+end
+
+M.kind_name = "file"
+
+return M
---
+++ spec/lua/thetto/handler/source/url/bookmark_spec.lua
@@ -0,0 +1,23 @@
+local helper = require("thetto.lib.testlib.helper")
+local thetto = helper.require("thetto")
+
+describe("url/bookmark source", function()
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
+  it("can show url bookmarks", function()
+    thetto.setup({
+      source = {
+        ["url/bookmark"] = {
+          opts = { file_path = helper.path("url_bookmark"), default_lines = { "https://example.com" } },
+        },
+      },
+    })
+
+    thetto.start("url/bookmark", { opts = { insert = false } })
+
+    assert.exists_pattern("https://example.com")
+
+    thetto.execute()
+  end)
+end)
