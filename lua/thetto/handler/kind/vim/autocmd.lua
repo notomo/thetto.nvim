6128693c79386bece9fb1a529541c039be438150
notomo <notomo.motono@gmail.com>
2022-04-06 09:49:32 +0900

    Cleanup autocmd source

--- lua/thetto/handler/kind/vim/autocmd.lua
+++ lua/thetto/handler/kind/vim/autocmd.lua
@@ -1,3 +1,28 @@
 local M = {}
 
-return require("thetto.core.kind").extend(M, "vim.anonymous_autocmd", "file")
+function M.action_delete_group(_, items)
+  for _, item in ipairs(items) do
+    vim.api.nvim_clear_autocmds({ group = item.autocmd.group })
+  end
+end
+
+function M.action_delete_autocmd_by_pattern(_, items)
+  for _, item in ipairs(items) do
+    vim.api.nvim_clear_autocmds({
+      group = item.autocmd.group,
+      event = item.autocmd.event,
+      pattern = item.autocmd.pattern,
+    })
+  end
+end
+
+function M.action_delete_autocmd_by_event(_, items)
+  for _, item in ipairs(items) do
+    vim.api.nvim_clear_autocmds({
+      group = item.autocmd.group,
+      event = item.autocmd.event,
+    })
+  end
+end
+
+return M
--- lua/thetto/handler/source/vim/autocmd.lua
+++ lua/thetto/handler/source/vim/autocmd.lua
@@ -1,132 +1,29 @@
-local pathlib = require("thetto.lib.path")
-
 local M = {}
 
-local Autocmd = {}
-Autocmd.__index = Autocmd
-
-function Autocmd.new(raw)
-  local autocmd = raw or {}
-  return setmetatable(autocmd, Autocmd)
-end
-
-function Autocmd.string(self)
-  return ("%s %s %s %s"):format(self.group, self.event, self.pattern, self.cmd)
-end
-
-function Autocmd.value(self)
-  return {
-    group = self.group,
-    event = self.event,
-    pattern = self.pattern,
-    cmd = self.cmd,
-    path = self.path,
-    row = self.row,
-  }
-end
-
-local Parser = {}
-Parser.__index = Parser
-
-function Parser.new(home)
-  local parser = {}
-  parser.autocmd = nil
-  parser.autocmds = {}
-  parser.home = home
-  return setmetatable(parser, Parser)
-end
-
-function Parser.eat(self, output)
-  -- ex. MyAuGroup  WinEnter
-  local group, event = output:match("^(%S+)%s+(%w+)$")
-  if group ~= nil and event ~= nil then
-    self.autocmd = Autocmd.new()
-    self.autocmd.group = group
-    self.autocmd.event = event
-    return
-  end
-
-  -- ex. WinEnter
-  event = output:match("^(%S+)$")
-  if event ~= nil then
-    self.autocmd = Autocmd.new()
-    self.autocmd.event = event
-    self.autocmd.group = "NoneGroup"
-    return
-  end
-
-  -- not supported lang=ja?
-  -- ex. \tLast set from /path/to/file line 8888
-  local path, row = output:match("^%s+Last%s+set%s+from%s+(%S+)%s+line%s+(%d+)")
-  if path ~= nil and row ~= nil then
-    self.autocmd.path = path:gsub("^~", self.home)
-    self.autocmd.row = tonumber(row)
-    if self.autocmd.event == nil or self.autocmd.pattern == nil or self.autocmd.cmd == nil then
-      local err = ("parse error: output=%s, current=%s"):format(output, vim.inspect(self.autocmd:value()))
-      error(err)
-    end
-    table.insert(self.autocmds, Autocmd.new(vim.deepcopy(self.autocmd)))
-    return
-  end
-
-  -- ex. Last set from anonymous :source line 8888
-  -- ex. Last set from anonymous :source
-  local source_row = output:match("^%s+Last%s+set%s+from%s+anonymous%s+:source.*")
-  if source_row ~= nil then
-    table.insert(self.autocmds, Autocmd.new(vim.deepcopy(self.autocmd)))
-    return
-  end
-
-  -- ex.    *         echomsg "executed"
-  local pattern, cmd = output:match("^    (%S+)%s+(.+)")
-  if pattern ~= nil and cmd ~= nil then
-    self.autocmd.pattern = pattern
-    self.autocmd.cmd = cmd
-    return
-  end
-
-  -- ex.    <buffer=8888>
-  pattern = output:match("^    (%S+)")
-  if pattern ~= nil then
-    self.autocmd.pattern = pattern
-    return
-  end
-
-  if self.autocmd.pattern ~= nil then
-    cmd = output:match("^%s+(.+)")
-    if cmd ~= nil then
-      self.autocmd.cmd = cmd
-      return
-    end
-    local err = ("parse error: output=%s, current=%s"):format(output, vim.inspect(self.autocmd:value()))
-    error(err)
-  end
-
-  local err = ("parse error: output=%s, current=%s"):format(output, vim.inspect(self.autocmd:value()))
-  error(err)
-end
-
-function M.collect(self)
-  local result = vim.api.nvim_exec("verbose autocmd", true)
-  local outputs = vim.split(result, "\n", true)
-  table.remove(outputs, 1)
-
+function M.collect()
   local items = {}
-  local parser = Parser.new(pathlib.home())
-  for _, output in ipairs(outputs) do
-    parser:eat(output)
-  end
-  for _, autocmd in ipairs(parser.autocmds) do
-    local kind_name
-    if not autocmd.path then
-      kind_name = "vim/anonymous_autocmd"
+  for _, autocmd in ipairs(vim.api.nvim_get_autocmds({})) do
+    local parts = {}
+    if autocmd.group then
+      table.insert(parts, autocmd.group)
     end
+
+    table.insert(parts, autocmd.event)
+    table.insert(parts, autocmd.pattern)
+
+    if autocmd.once then
+      table.insert(parts, "++once")
+    end
+
+    if autocmd.command then
+      table.insert(parts, autocmd.command)
+    elseif autocmd.desc then
+      table.insert(parts, autocmd.desc)
+    end
+
     table.insert(items, {
-      value = autocmd:string(),
-      autocmd = autocmd:value(),
-      path = autocmd.path,
-      row = autocmd.row,
-      kind_name = kind_name,
+      value = table.concat(parts, " "),
+      autocmd = autocmd,
     })
   end
   return items
