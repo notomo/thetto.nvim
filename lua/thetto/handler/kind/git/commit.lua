42f301ab84b40dad6aed9b54de87b15fff6e5ae2
notomo <notomo.motono@gmail.com>
2022-06-14 09:56:48 +0900

    Add git/commit open actions

--- lua/thetto/handler/kind/git/commit.lua
+++ lua/thetto/handler/kind/git/commit.lua
@@ -1,18 +1,12 @@
 local M = {}
 
-function M.action_preview(self, items, ctx)
-  local item = items[1]
-  if not item then
-    return nil
-  end
-
-  local bufnr = vim.api.nvim_create_buf(false, true)
+local start = function(source, bufnr, item)
   vim.bo[bufnr].bufhidden = "wipe"
   vim.bo[bufnr].filetype = "diff"
 
   local cmd = { "git", "show", "--date=iso", item.commit_hash }
 
-  local job = self.jobs.new(cmd, {
+  local job = source.jobs.new(cmd, {
     on_exit = function(job_self)
       if not vim.api.nvim_buf_is_valid(bufnr) then
         return
@@ -20,16 +14,61 @@ function M.action_preview(self, items, ctx)
       local lines = job_self:get_output()
       vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
     end,
-    on_stderr = self.jobs.print_stderr,
+    on_stderr = source.jobs.print_stderr,
   })
   local err = job:start()
   if err ~= nil then
     return nil, err
   end
-
-  ctx.ui:open_preview(item, { raw_bufnr = bufnr })
+  return job, nil
 end
 
-M.default_action = "show"
+local open = function(source, items, f)
+  for _, item in ipairs(items) do
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    local _, err = start(source, bufnr, item)
+    if err then
+      return nil, err
+    end
+    f(bufnr)
+  end
+end
+
+function M.action_open(self, items)
+  return open(self, items, function(bufnr)
+    vim.cmd([[buffer ]] .. bufnr)
+  end)
+end
+
+function M.action_vsplit_open(self, items)
+  return open(self, items, function(bufnr)
+    vim.cmd([[vsplit | buffer ]] .. bufnr)
+  end)
+end
+
+function M.action_tab_open(self, items)
+  return open(self, items, function(bufnr)
+    vim.cmd([[tabedit | buffer ]] .. bufnr)
+  end)
+end
+
+function M.action_preview(self, items, ctx)
+  local item = items[1]
+  if not item then
+    return nil
+  end
+
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  local job, err = start(self, bufnr, item)
+  if err ~= nil then
+    return nil, err
+  end
+
+  ctx.ui:open_preview(item, { raw_bufnr = bufnr })
+
+  return job, nil
+end
+
+M.default_action = "open"
 
 return M
