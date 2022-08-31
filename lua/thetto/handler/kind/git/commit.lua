1f36064b56dd92e422dd5e273959248ae3597995
notomo <notomo.motono@gmail.com>
2022-08-31 21:47:16 +0900

    Fix source.jobs

--- lua/thetto/handler/kind/git/commit.lua
+++ lua/thetto/handler/kind/git/commit.lua
@@ -1,12 +1,12 @@
 local M = {}
 
-local start = function(source, bufnr, item)
+local start = function(bufnr, item)
   vim.bo[bufnr].bufhidden = "wipe"
   vim.bo[bufnr].filetype = "diff"
 
   local cmd = { "git", "show", "--date=iso", item.commit_hash }
 
-  local job = source.jobs.new(cmd, {
+  local job = require("thetto.lib.job").new(cmd, {
     on_exit = function(job_self)
       if not vim.api.nvim_buf_is_valid(bufnr) then
         return
@@ -14,7 +14,7 @@ local start = function(source, bufnr, item)
       local lines = job_self:get_output()
       vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
     end,
-    on_stderr = source.jobs.print_stderr,
+    on_stderr = require("thetto.lib.job").print_stderr,
   })
   local err = job:start()
   if err ~= nil then
@@ -23,10 +23,10 @@ local start = function(source, bufnr, item)
   return job, nil
 end
 
-local open = function(source, items, f)
+local open = function(items, f)
   for _, item in ipairs(items) do
     local bufnr = vim.api.nvim_create_buf(false, true)
-    local _, err = start(source, bufnr, item)
+    local _, err = start(bufnr, item)
     if err then
       return nil, err
     end
@@ -34,34 +34,34 @@ local open = function(source, items, f)
   end
 end
 
-function M.action_open(self, items)
-  return open(self, items, function(bufnr)
+function M.action_open(_, items)
+  return open(items, function(bufnr)
     vim.cmd.buffer({ count = bufnr })
   end)
 end
 
-function M.action_vsplit_open(self, items)
-  return open(self, items, function(bufnr)
+function M.action_vsplit_open(_, items)
+  return open(items, function(bufnr)
     vim.cmd.vsplit()
     vim.cmd.buffer({ count = bufnr })
   end)
 end
 
-function M.action_tab_open(self, items)
-  return open(self, items, function(bufnr)
+function M.action_tab_open(_, items)
+  return open(items, function(bufnr)
     vim.cmd.tabedit()
     vim.cmd.buffer({ count = bufnr })
   end)
 end
 
-function M.action_preview(self, items, ctx)
+function M.action_preview(_, items, ctx)
   local item = items[1]
   if not item then
     return nil
   end
 
   local bufnr = vim.api.nvim_create_buf(false, true)
-  local job, err = start(self, bufnr, item)
+  local job, err = start(bufnr, item)
   if err ~= nil then
     return nil, err
   end
