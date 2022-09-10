b5bd5191a2d5b2f296103a08c41fb84fb49389ff
notomo <notomo.motono@gmail.com>
2022-09-10 22:31:25 +0900

    Remove unused word action

--- lua/thetto/handler/kind/word.lua
+++ lua/thetto/handler/kind/word.lua
@@ -1,11 +1,5 @@
 local M = {}
 
-function M.action_echo(items)
-  for _, item in ipairs(items) do
-    vim.api.nvim_out_write(item.value .. "\n")
-  end
-end
-
-M.default_action = "echo"
+-- alias to base
 
 return M
