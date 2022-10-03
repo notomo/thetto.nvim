f44e4a52af5d71c9f4651d078c91b80cfc39a19e
notomo <notomo.motono@gmail.com>
2022-10-03 00:54:45 +0000

    Add lsp/codelens source

---
+++ lua/thetto/handler/source/vim/lsp/codelens.lua
@@ -0,0 +1,30 @@
+local util = require("thetto.util.lsp")
+
+local M = {}
+
+function M.collect(source_ctx)
+  return function(observer)
+    local method = "textDocument/codeLens"
+    local params = { textDocument = vim.lsp.util.make_text_document_params(source_ctx.bufnr) }
+    local _, cancel = vim.lsp.buf_request(source_ctx.bufnr, method, params, function(...)
+      vim.lsp.codelens.on_codelens(...)
+
+      local items = {}
+      for _, codelens in ipairs(vim.lsp.codelens.get(source_ctx.bufnr)) do
+        table.insert(items, {
+          value = codelens.command.title,
+          bufnr = source_ctx.bufnr,
+          row = codelens.range.start.line + 1,
+          range = util.range(codelens.range),
+        })
+      end
+      observer:next(items)
+      observer:complete()
+    end)
+    return cancel
+  end
+end
+
+M.kind_name = "position"
+
+return M
