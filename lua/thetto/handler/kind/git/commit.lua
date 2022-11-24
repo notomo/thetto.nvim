d9b6a3fd2ed684511064db8383425f50ba171dd7
notomo <notomo.motono@gmail.com>
2022-11-24 21:16:13 +0900

    Add git/commit actions

--- lua/thetto/handler/kind/git/commit.lua
+++ lua/thetto/handler/kind/git/commit.lua
@@ -1,5 +1,11 @@
 local M = {}
 
+M.behaviors = {
+  fixup = { quit = false },
+  reset = { quit = false },
+  checkout = { quit = false },
+}
+
 local open_diff = require("thetto.handler.kind.git._util").open_diff
 
 function M.action_open(items)
@@ -35,6 +41,47 @@ function M.action_preview(items, _, ctx)
     return nil, err
   end
   return promise
+end
+
+function M.action_fixup(items)
+  local item = items[1]
+  if not item then
+    return nil
+  end
+  local bufnr = vim.api.nvim_get_current_buf()
+  return require("thetto.util.job").promise({ "git", "commit", "--fixup=" .. item.commit_hash }):next(function()
+    return require("thetto.command").reload(bufnr)
+  end)
+end
+
+function M.action_rebase_interactively(items)
+  local item = items[1]
+  if not item then
+    return nil
+  end
+  return require("thetto.util.job").promise({ "git", "rebase", "-i", "--autosquash", item.commit_hash .. "~" })
+end
+
+function M.action_reset(items)
+  local item = items[1]
+  if not item then
+    return nil
+  end
+  local bufnr = vim.api.nvim_get_current_buf()
+  return require("thetto.util.job").promise({ "git", "reset", item.commit_hash }):next(function()
+    return require("thetto.command").reload(bufnr)
+  end)
+end
+
+function M.action_checkout(items)
+  local item = items[1]
+  if not item then
+    return nil
+  end
+  local bufnr = vim.api.nvim_get_current_buf()
+  return require("thetto.util.job").promise({ "git", "checkout", item.commit_hash }):next(function()
+    return require("thetto.command").reload(bufnr)
+  end)
 end
 
 M.default_action = "open"
