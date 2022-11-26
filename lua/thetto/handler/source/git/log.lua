d7ed91646367ddd00b741a44c584c0851d1f9792
notomo <notomo.motono@gmail.com>
2022-11-26 11:16:39 +0900

    Improve git/log highlight

--- lua/thetto/handler/source/git/log.lua
+++ lua/thetto/handler/source/git/log.lua
@@ -16,20 +16,24 @@ function M.collect(source_ctx)
     "git",
     "--no-pager",
     "log",
-    "--pretty=format:%h %s <%an>%d",
+    "--pretty=format:%h\t\t<%an>\t\t<%s>\t\t<%d>",
     "--",
     unpack(source_ctx.opts.paths),
   }
   return require("thetto.util.job").start(cmd, source_ctx, function(output)
-    local commit_hash = output:match("^(%S+) ")
+    local commit_hash, user_name, message, branch_info = output:match("^(%S+)\t\t(<.*>)\t\t<(.*)>\t\t<(.*)>")
     if not commit_hash then
       return nil
     end
+    local value = ("%s %s %s%s"):format(commit_hash, message, user_name, branch_info)
     return {
-      value = output,
+      value = value,
       commit_hash = commit_hash,
       column_offsets = {
         commit_hash = 0,
+        message = #commit_hash + 1,
+        user_name = #commit_hash + 1 + #message + 1,
+        branch_info = #commit_hash + 1 + #message + 1 + #user_name + 1,
       },
     }
   end)
@@ -37,8 +41,17 @@ end
 
 M.highlight = require("thetto.util.highlight").columns({
   {
-    group = "Character",
-    end_column = 8,
+    group = "Comment",
+    end_key = "message",
+  },
+  {
+    group = "Label",
+    start_key = "user_name",
+    end_key = "branch_info",
+  },
+  {
+    group = "Conditional",
+    start_key = "branch_info",
   },
 })
 
