ab6a26dac786474652dc4d9b74a8d0921526764d
notomo <notomo.motono@gmail.com>
2022-12-02 10:02:44 +0900

    Improve git/file_log to follow rename

--- lua/thetto/handler/source/git/log.lua
+++ lua/thetto/handler/source/git/log.lua
@@ -3,6 +3,7 @@ local filelib = require("thetto.lib.file")
 local M = {}
 
 M.opts = {
+  args = {},
   paths = {},
 }
 
@@ -17,9 +18,9 @@ function M.collect(source_ctx)
     "--no-pager",
     "log",
     "--pretty=format:%h\t\t<%an>\t\t<%s>\t\t<%d>",
-    "--",
-    unpack(source_ctx.opts.paths),
   }
+  vim.list_extend(cmd, source_ctx.opts.args)
+  vim.list_extend(cmd, { "--", unpack(source_ctx.opts.paths) })
   return require("thetto.util.job").start(cmd, source_ctx, function(output)
     local commit_hash, user_name, message, branch_info = output:match("^(%S+)\t\t(<.*>)\t\t<(.*)>\t\t<(.*)>")
     if not commit_hash then
