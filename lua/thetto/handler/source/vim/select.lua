fa1503147e1cda15783e36b4d20a99d349395f60
notomo <notomo.motono@gmail.com>
2022-12-06 20:49:50 +0900

    Add prompt message to vim/select source

--- lua/thetto/handler/source/vim/select.lua
+++ lua/thetto/handler/source/vim/select.lua
@@ -13,14 +13,26 @@ function M.collect(source_ctx)
   local items = {}
   for _, item in ipairs(source_ctx.opts.items) do
     local value = source_ctx.opts.format_item(item)
+    local desc = ("%s %s"):format(source_ctx.opts.prompt, value)
     table.insert(items, {
       value = value,
+      desc = desc,
       raw = item,
       on_choice = source_ctx.opts.on_choice,
+      column_offsets = {
+        value = #source_ctx.opts.prompt + 1,
+      },
     })
   end
   return items
 end
+
+M.highlight = require("thetto.util.highlight").columns({
+  {
+    group = "Comment",
+    end_key = "value",
+  },
+})
 
 M.actions = {
   action_choose = function(items)
