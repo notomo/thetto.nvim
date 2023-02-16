33d6023cfe69a4dabca01671b97eb053f56ecd88
notomo <notomo.motono@gmail.com>
2023-02-16 09:44:23 +0900

    Impl windows process kill action

--- lua/thetto/handler/kind/env/process.lua
+++ lua/thetto/handler/kind/env/process.lua
@@ -1,17 +1,28 @@
 local M = {}
 
-M.cmd = { "kill" }
+if vim.fn.has("win32") == 1 then
+  M.get_cmds = function(pids)
+    return vim.tbl_map(function(pid)
+      return { "taskkill", "/pid", pid, "/F" }
+    end, pids)
+  end
+else
+  M.get_cmds = function(pids)
+    local cmd = { "kill" }
+    vim.list_extend(cmd, pids)
+    return { cmd }
+  end
+end
 
 function M.action_kill(items)
-  local pids = {}
-  for _, item in ipairs(items) do
-    table.insert(pids, item.pid)
-  end
+  local pids = vim.tbl_map(function(item)
+    return item.pid
+  end, items)
 
-  local cmd = vim.deepcopy(M.cmd)
-  vim.list_extend(cmd, pids)
-
-  return require("thetto.util.job").promise(cmd)
+  local cmds = M.get_cmds(pids)
+  return require("thetto.vendor.promise").all_settled(vim.tbl_map(function(cmd)
+    return require("thetto.util.job").promise(cmd)
+  end, cmds))
 end
 
 return M
