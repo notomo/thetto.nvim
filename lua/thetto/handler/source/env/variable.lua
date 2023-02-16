859c94a346036af66bdf81a7d1122761445d7c70
notomo <notomo.motono@gmail.com>
2023-02-16 22:54:56 +0900

    Fix env/variable source with value including newline

--- lua/thetto/handler/source/env/variable.lua
+++ lua/thetto/handler/source/env/variable.lua
@@ -4,7 +4,7 @@ function M.collect()
   local names = vim.fn.getcompletion("*", "environment")
   local items = {}
   for _, name in ipairs(names) do
-    local value = ("%s=%s"):format(name, os.getenv(name))
+    local value = ("%s=%s"):format(name, os.getenv(name):gsub("\n", "\\n"))
     table.insert(items, { value = value })
   end
   return items
