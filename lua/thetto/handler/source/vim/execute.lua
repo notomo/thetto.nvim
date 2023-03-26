cf44311848612771707d9aaaf3b81402e6d84de9
notomo <notomo.motono@gmail.com>
2023-03-26 18:54:19 +0900

    Use nvim_exec2()

--- lua/thetto/handler/source/vim/execute.lua
+++ lua/thetto/handler/source/vim/execute.lua
@@ -4,7 +4,7 @@ M.opts = { cmd = "messages" }
 
 function M.collect(source_ctx)
   local items = {}
-  local outputs = vim.api.nvim_exec(source_ctx.opts.cmd, true)
+  local outputs = vim.api.nvim_exec2(source_ctx.opts.cmd, { output = true }).output
   for line in vim.gsplit(outputs, "\n", true) do
     table.insert(items, { value = line })
   end
