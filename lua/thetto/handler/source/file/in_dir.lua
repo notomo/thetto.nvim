8d4afd785dcba423790f49fb3f164d474bb1e55e
notomo <notomo.motono@gmail.com>
2023-07-10 21:15:28 +0900

    Small refactor file/in_dir source

--- lua/thetto/handler/source/file/in_dir.lua
+++ lua/thetto/handler/source/file/in_dir.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.collect(source_ctx)
   local paths = {}
   for _, path in ipairs(vim.fn.readdir(source_ctx.cwd)) do
-    local abs_path = vim.fn.fnamemodify(source_ctx.cwd .. "/" .. path, ":p:gs?\\?\\/?")
+    local abs_path = vim.fs.normalize(vim.fs.joinpath(source_ctx.cwd, path))
     table.insert(paths, abs_path)
   end
 
@@ -18,15 +18,17 @@ function M.collect(source_ctx)
 
   local items = {}
   for _, path in ipairs(paths) do
-    local value
+    local value = vim.fs.basename(path)
     local kind_name = M.kind_name
     if vim.fn.isdirectory(path) ~= 0 then
-      value = vim.fn.fnamemodify(path, ":h:t") .. "/"
+      value = value .. "/"
       kind_name = "file/directory"
-    else
-      value = vim.fn.fnamemodify(path, ":t")
     end
-    table.insert(items, { value = value, path = path, kind_name = kind_name })
+    table.insert(items, {
+      value = value,
+      path = path,
+      kind_name = kind_name,
+    })
   end
   return items
 end
