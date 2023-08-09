59fe22144e57c51b430f28ec708abb52970ccbca
notomo <notomo.motono@gmail.com>
2023-08-10 08:50:55 +0900

    Use vim.ui.open()

--- lua/thetto/handler/kind/url.lua
+++ lua/thetto/handler/kind/url.lua
@@ -1,7 +1,19 @@
 local M = {}
 
-function M.action_open_browser(_, _)
-  error("not implemented")
+function M.action_open_browser(items)
+  local errs = {}
+  for _, item in ipairs(items) do
+    local url = item.url
+    if not vim.startswith(url, "http") then
+      table.insert(errs, "invalid url: " .. url)
+    else
+      vim.ui.open(url)
+    end
+  end
+  if #errs == 0 then
+    return
+  end
+  return nil, table.concat(errs, "\n")
 end
 
 M.default_action = "open_browser"
