d92633e0283caffa0fbc720efbf48d14cc8fb91c
notomo <notomo.motono@gmail.com>
2023-09-18 15:00:08 +0900

    Add github/issue close,reopen action

--- lua/thetto/handler/kind/github/issue.lua
+++ lua/thetto/handler/kind/github/issue.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+M.opts = {}
+
 function M.action_edit_last_comment(items)
   local item = items[1]
   if not item then
@@ -7,7 +9,7 @@ function M.action_edit_last_comment(items)
   end
 
   local cmd = { "gh", "issue", "comment", item.url, "--editor", "--edit-last" }
-  return require("thetto.util.job").promise(cmd)
+  return require("thetto.util.job").execute(cmd)
 end
 
 function M.action_comment(items)
@@ -17,7 +19,56 @@ function M.action_comment(items)
   end
 
   local cmd = { "gh", "issue", "comment", item.url, "--editor" }
-  return require("thetto.util.job").promise(cmd)
+  return require("thetto.util.job").execute(cmd)
+end
+
+M.opts.close = {
+  reason = "completed",
+  comment = "",
+}
+function M.action_close(items, action_ctx)
+  local item = items[1]
+  if not item then
+    return nil, "no item"
+  end
+
+  local cmd = { "gh", "issue", "close", item.url, "--reason", action_ctx.opts.reason }
+  if action_ctx.opts.comment ~= "" then
+    table.insert(cmd, "--comment=" .. action_ctx.opts.comment)
+  end
+
+  return require("thetto.util.job").execute(cmd)
+end
+
+function M.action_close_with_comment(items, action_ctx, ctx)
+  return require("thetto.util.input")
+    .promise({
+      prompt = "Comment: ",
+    })
+    :next(function(input)
+      if not input or input == "" then
+        return require("thetto.vendor.misclib.message").info("Canceled issue close with comment")
+      end
+      return require("thetto.util.action").call(action_ctx.kind_name, "close", items, ctx, {
+        comment = input,
+      })
+    end)
+end
+
+function M.action_close_not_planned(items, action_ctx, ctx)
+  return require("thetto.util.action").call(action_ctx.kind_name, "close", items, ctx, {
+    reason = { "not planned" },
+  })
+end
+
+function M.action_reopen(items)
+  local item = items[1]
+  if not item then
+    return nil, "no item"
+  end
+
+  local cmd = { "gh", "issue", "reopen", item.url }
+  return require("thetto.util.job").execute(cmd)
 end
 
 return require("thetto.core.kind").extend(M, "url")
