aba815512b03752507ba6335f4bfcfaab077c2c0
notomo <notomo.motono@gmail.com>
2024-01-06 15:18:26 +0900

    filter option

--- lua/thetto2/handler/pipeline/filter/substring.lua
+++ lua/thetto2/handler/pipeline/filter/substring.lua
@@ -2,9 +2,9 @@ local vim = vim
 
 local M = {}
 
-local to_texts = function(stage_ctx, opts)
+local to_texts = function(stage_ctx, ignorecase)
   local input = stage_ctx.input
-  if opts.ignorecase then
+  if ignorecase then
     input = stage_ctx.input:lower()
   end
   return vim.tbl_filter(function(text)
@@ -12,22 +12,25 @@ local to_texts = function(stage_ctx, opts)
   end, vim.split(input, "%s"))
 end
 
+local is_ignorecase = require("thetto2.util.pipeline").is_ignorecase
+
 function M.apply(stage_ctx, items, opts)
   if stage_ctx.input == "" then
     return items
   end
 
   local filtered = {}
-  local texts = to_texts(stage_ctx, opts)
+  local ignorecase = is_ignorecase(opts.ignorecase, opts.smartcase, stage_ctx.input)
+  local texts = to_texts(stage_ctx, ignorecase)
   for _, item in ipairs(items) do
-    local value = item.value
-    if opts.ignorecase then
-      value = value:lower()
+    local field = opts.to_field(item)
+    if ignorecase then
+      field = field:lower()
     end
 
     local ok = true
     for _, text in ipairs(texts) do
-      if (value:find(text, 1, true) ~= nil) == opts.inversed then
+      if (field:find(text, 1, true) ~= nil) == opts.inversed then
         ok = false
         break
       end
@@ -41,8 +44,12 @@ function M.apply(stage_ctx, items, opts)
 end
 
 M.opts = {
+  smartcase = true,
   ignorecase = false,
   inversed = false,
+  to_field = function(item)
+    return item.value
+  end,
 }
 
 return M
--- lua/thetto2/util/pipeline.lua
+++ lua/thetto2/util/pipeline.lua
@@ -13,9 +13,29 @@ function M.by_names(names)
       return require("thetto2.handler.pipeline." .. name)
     end)
     :totable()
+  return M.list(stages)
+end
+
+function M.list(stages)
   return function()
     return require("thetto2.core.pipeline").new(stages)
   end
+end
+
+function M.filter(name, fields)
+  local origin = require("thetto2.vendor.misclib.module").find("thetto2.handler.pipeline.filter." .. name)
+  if not origin then
+    error("not found filter: " .. name)
+  end
+
+  local filter = vim.tbl_deep_extend("force", vim.deepcopy(origin), fields or {})
+  filter.name = name
+  return filter
+end
+
+function M.is_ignorecase(ignorecase, smartcase, input)
+  local case_sensitive = not ignorecase and smartcase and input:find("[A-Z]")
+  return not case_sensitive
 end
 
 return M
