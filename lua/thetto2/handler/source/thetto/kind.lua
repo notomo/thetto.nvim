ecb0cb04d51d9808559bbbb49c357be8f9952176
notomo <notomo.motono@gmail.com>
2024-01-08 18:32:57 +0900

    thetto/kind

--- lua/thetto2/handler/source/thetto/kind.lua
+++ lua/thetto2/handler/source/thetto/kind.lua
@@ -1,18 +1,36 @@
 local M = {}
 
+local get_all_kind = function()
+  local paths = vim.api.nvim_get_runtime_file("lua/thetto2/handler/kind/**/*.lua", true)
+  local already = {}
+  local all = {}
+  for _, path in ipairs(paths) do
+    local kind_file =
+      vim.split(require("thetto2.lib.path").adjust_sep(path), "lua/thetto2/handler/kind/", { plain = true })[2]
+    local name = kind_file:sub(1, #kind_file - 4)
+    local ignored = vim.startswith(vim.fs.basename(kind_file), "_")
+    if not ignored and not already[name] then
+      local kind_info = {
+        name = name,
+        path = path,
+      }
+      table.insert(all, kind_info)
+      already[name] = kind_info
+    end
+  end
+  return all
+end
+
 function M.collect()
-  local items = {}
-  local all_kinds, err = require("thetto2.core.kind").all()
-  if err then
-    return nil, err
-  end
-  for _, e in ipairs(all_kinds) do
-    table.insert(items, {
-      value = e.name,
-      path = e.path,
-    })
-  end
-  return items
+  return vim
+    .iter(get_all_kind())
+    :map(function(e)
+      return {
+        value = e.name,
+        path = e.path,
+      }
+    end)
+    :totable()
 end
 
 M.kind_name = "file"
