4bdcfe62fe38e155135a9a0602d16b5a59f87583
notomo <notomo.motono@gmail.com>
2024-01-14 13:27:27 +0900

    Fix ui util

--- lua/thetto2/util/consumer.lua
+++ lua/thetto2/util/consumer.lua
@@ -3,15 +3,15 @@ local M = {}
 function M.ui(raw_opts)
   raw_opts = raw_opts or {}
   return function(consumer_ctx, source, pipeline, callbacks, actions)
+    local opts_from_source = vim.tbl_get(source, "consumer_opts", "ui") or {}
+    local opts = vim.tbl_deep_extend("force", raw_opts, opts_from_source)
+
     local kind = require("thetto2.core.kind").by_name(source.kind_name or "base", actions)
-    if raw_opts.has_sidecar == nil then
-      raw_opts.has_sidecar = require("thetto2.core.kind").can_preview(kind)
+    if opts.has_sidecar == nil then
+      opts.has_sidecar = require("thetto2.core.kind").can_preview(kind)
     end
 
-    local opts_from_source = vim.tbl_get(source, "consumer_opts", "ui") or {}
-    raw_opts = vim.tbl_deep_extend("force", raw_opts, opts_from_source)
-
-    return require("thetto2.handler.consumer.ui").new(consumer_ctx, source, pipeline, callbacks, actions, raw_opts)
+    return require("thetto2.handler.consumer.ui").new(consumer_ctx, source, pipeline, callbacks, actions, opts)
   end
 end
 
