bbf4906f963497230a5f84f769fd738df7a3d808
notomo <notomo.motono@gmail.com>
2024-01-18 21:58:46 +0900

    Fix vendor

--- spec/lua/thetto/vendorlib.lua
+++ spec/lua/thetto/vendorlib.lua
@@ -1,10 +1,15 @@
 return {
+  "notomo/misclib.nvim/lua/misclib/collection/list.lua",
+  "notomo/misclib.nvim/lua/misclib/cursor.lua",
+  "notomo/misclib.nvim/lua/misclib/decorator.lua",
+  "notomo/misclib.nvim/lua/misclib/highlight.lua",
+  "notomo/misclib.nvim/lua/misclib/job/init.lua",
+  "notomo/misclib.nvim/lua/misclib/job/output.lua",
+  "notomo/misclib.nvim/lua/misclib/message.lua",
+  "notomo/misclib.nvim/lua/misclib/module.lua",
   "notomo/misclib.nvim/lua/misclib/observable.lua",
   "notomo/misclib.nvim/lua/misclib/test/data_dir.lua",
   "notomo/misclib.nvim/lua/misclib/visual_mode.lua",
-  "notomo/misclib.nvim/lua/misclib/highlight.lua",
   "notomo/misclib.nvim/lua/misclib/window.lua",
-  "notomo/misclib.nvim/lua/misclib/decorator.lua",
   "notomo/promise.nvim/lua/promise/init.lua",
-  "notomo/misclib.nvim/lua/misclib/collection/list.lua",
 }
