9ca19c06945507ff84f2bd7bebd2657e91cf8483
notomo <notomo.motono@gmail.com>
2024-01-18 21:45:36 +0900

    Fix git/file_log

--- lua/thetto/handler/source/git/file_log.lua
+++ lua/thetto/handler/source/git/file_log.lua
@@ -1,10 +1,14 @@
 local M = {}
 
 function M.collect(source_ctx)
-  return require("thetto.handler.source.git.log").collect(source_ctx:change_opts({
-    args = { "--follow" },
-    path = vim.api.nvim_buf_get_name(source_ctx.bufnr),
-  }))
+  local source = require("thetto.util.source").by_name("git/log", {
+    opts = {
+      args = { "--follow" },
+      path = vim.api.nvim_buf_get_name(source_ctx.bufnr),
+    },
+  })
+  local new_source_ctx = vim.tbl_deep_extend("force", source_ctx, { opts = source.opts })
+  return source.collect(new_source_ctx)
 end
 
 M.highlight = require("thetto.handler.source.git.log").highlight
