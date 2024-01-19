90b38e85761deb1657296d22456e9537e3a6c8da
notomo <notomo.motono@gmail.com>
2024-01-19 21:45:00 +0900

    Small refactor

--- lua/thetto/core/pipeline_context.lua
+++ lua/thetto/core/pipeline_context.lua
@@ -1,11 +1,10 @@
 local M = {}
 M.__index = M
 
-function M.new(inputs, source_input, is_interactive)
+function M.new(inputs, source_input)
   local tbl = {
     inputs = inputs,
     source_input = source_input,
-    is_interactive = is_interactive,
   }
   return setmetatable(tbl, M)
 end
