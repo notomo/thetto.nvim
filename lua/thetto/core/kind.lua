7a535cc0722a812021e9656495a3c10d8375336d
notomo <notomo.motono@gmail.com>
2024-01-21 11:26:17 +0900

    Refactor base kind

--- lua/thetto/core/kind.lua
+++ lua/thetto/core/kind.lua
@@ -1,6 +1,24 @@
-local base = require("thetto.handler.kind.base")
-
 local M = {}
+
+local ACTION_PREFIX = "action_"
+function M._action_name_to_kind_name_map(kind_name, kind)
+  local action_name_to_kind_name = {}
+  vim
+    .iter(kind)
+    :map(function(k)
+      if not vim.startswith(k, ACTION_PREFIX) then
+        return nil
+      end
+      return k
+    end)
+    :each(function(k)
+      action_name_to_kind_name[k] = kind_name
+    end)
+  return vim.tbl_extend("keep", kind._action_name_to_kind_name or {}, action_name_to_kind_name)
+end
+
+local base = require("thetto.handler.kind.base")
+base._action_name_to_kind_name = M._action_name_to_kind_name_map("base", base)
 
 local _registered = {}
 
@@ -15,7 +33,7 @@ function M.by_name(kind_name, fields, raw_opts)
     if type(registered) == "function" then
       registered = registered()
     end
-    return vim.tbl_deep_extend("force", vim.deepcopy(registered), fields or {})
+    return vim.tbl_deep_extend("force", base, vim.deepcopy(registered), fields or {})
   end
 
   local origin = require("thetto.vendor.misclib.module").find("thetto.handler.kind." .. kind_name)
@@ -23,7 +41,7 @@ function M.by_name(kind_name, fields, raw_opts)
     error("not found kind: " .. kind_name)
   end
 
-  local kind = vim.tbl_deep_extend("force", vim.deepcopy(origin), fields or {})
+  local kind = vim.tbl_deep_extend("force", base, vim.deepcopy(origin), fields or {})
   kind.name = kind_name
   kind._action_name_to_kind_name = kind._action_name_to_kind_name or {}
 
@@ -34,13 +52,14 @@ function M.register(kind_name, kind)
   _registered[kind_name] = kind
 end
 
-local ACTION_PREFIX = "action_"
 local action_key = function(kind, raw_action_name)
-  local name = raw_action_name
-  if name == "default" then
-    name = kind.default_action
+  local action_name
+  if action_name == "default" then
+    action_name = kind.default_action
+  else
+    action_name = raw_action_name
   end
-  return ACTION_PREFIX .. name, name
+  return ACTION_PREFIX .. action_name, action_name
 end
 
 function M.find_action(kind, raw_action_name)
@@ -51,11 +70,6 @@ function M.find_action(kind, raw_action_name)
   end
 
   local action_opts = kind.opts[action_name] or {}
-  if kind.name == "base" then
-    local base_action_opts = base.opts[action_name] or {}
-    action_opts = vim.tbl_deep_extend("force", base_action_opts, action_opts)
-  end
-
   return function(items, raw_action_ctx)
     local action_ctx = vim.tbl_deep_extend("force", { opts = action_opts }, raw_action_ctx)
     return action(items, action_ctx)
@@ -64,16 +78,12 @@ end
 
 function M.action_kind_name(kind, action_name)
   local key = action_key(kind, action_name)
-  if rawget(kind, key) then
-    return kind.name
-  end
+
   local extend_kind_name = kind._action_name_to_kind_name[key]
   if extend_kind_name then
     return extend_kind_name
   end
-  if base[key] then
-    return "base"
-  end
+
   return kind.name
 end
 
@@ -93,22 +103,6 @@ function M.can_preview(kind)
   return kind.get_preview ~= nil
 end
 
-function M._action_name_to_kind_name_map(kind_name, kind)
-  local action_name_to_kind_name = {}
-  vim
-    .iter(kind)
-    :map(function(k)
-      if not vim.startswith(k, ACTION_PREFIX) then
-        return nil
-      end
-      return k
-    end)
-    :each(function(k)
-      action_name_to_kind_name[k] = kind_name
-    end)
-  return action_name_to_kind_name
-end
-
 function M.extend(kind, ...)
   local extends = vim
     .iter({ ... })
@@ -118,35 +112,31 @@ function M.extend(kind, ...)
       return extend
     end)
     :totable()
-  return vim.tbl_deep_extend("keep", kind, unpack(extends))
+  local extended = vim.tbl_deep_extend("keep", kind, unpack(extends))
+  return extended
 end
 
 function M.action_infos(kind)
   local already = {}
-  local to_action_infos = function(from, actions)
-    return vim
-      .iter(actions)
-      :map(function(key)
-        if already[key] then
-          return
-        end
-        if not vim.startswith(key, ACTION_PREFIX) then
-          return
-        end
-        local action_name = key:gsub("^" .. ACTION_PREFIX, "")
-        already[key] = true
-        return {
-          from = from,
-          name = action_name,
-        }
-      end)
-      :totable()
-  end
+  return vim
+    .iter(kind)
+    :map(function(key)
+      if already[key] then
+        return
+      end
 
-  local action_infos = {}
-  vim.list_extend(action_infos, to_action_infos(kind.name, kind))
-  vim.list_extend(action_infos, to_action_infos("base", base))
-  return action_infos
+      if not vim.startswith(key, ACTION_PREFIX) then
+        return
+      end
+      already[key] = true
+
+      local action_name = key:gsub("^" .. ACTION_PREFIX, "")
+      return {
+        from = M.action_kind_name(kind, action_name),
+        name = action_name,
+      }
+    end)
+    :totable()
 end
 
 function M.registered_names()
