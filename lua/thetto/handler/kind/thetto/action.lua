df7ec70506a7001fec2bbe7d61836b64607cbd6f
notomo <notomo.motono@gmail.com>
2024-01-27 15:11:56 +0900

    Use action util

--- lua/thetto/handler/kind/thetto/action.lua
+++ lua/thetto/handler/kind/thetto/action.lua
@@ -2,11 +2,10 @@ local M = {}
 
 function M.action_execute(items)
   for _, item in ipairs(items) do
-    local item_action_groups = require("thetto.util.action").grouping({ item.item }, {
-      action_name = item.value,
-      actions = item.metadata.actions,
-    })
-    require("thetto").execute(item_action_groups, { quit = false })
+    local action_name = item.value
+    require("thetto.util.action").execute(action_name, {}, { quit = false }, function()
+      return { item.item }, item.metadata
+    end)
   end
 end
 
