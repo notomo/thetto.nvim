28fc4d021981ca9b5ba2692198f53429e978b424
notomo <notomo.motono@gmail.com>
2024-03-11 08:57:15 +0900

    Add lsp show_capabilities, show_server_capabilities actions

--- lua/thetto/handler/source/vim/lsp/client.lua
+++ lua/thetto/handler/source/vim/lsp/client.lua
@@ -12,6 +12,7 @@ function M.collect()
     local desc = ("%d %s %s"):format(client.id, client.name, vim.inspect(config, { newline = " ", indent = "" }))
     table.insert(items, {
       value = client.name,
+      client_id = client.id,
       desc = desc,
       column_offsets = {
         value = #tostring(client.id) + 1,
@@ -24,5 +25,43 @@ end
 M.kind_name = "word"
 
 M.cwd = require("thetto.util.cwd").project()
+
+M.actions = {
+  action_show_server_capabilities = function(items)
+    local item = items[1]
+    if not item then
+      return
+    end
+
+    local client = vim.lsp.get_clients({ id = item.client_id })[1]
+    if not item then
+      return
+    end
+
+    local content = vim.inspect(client.server_capabilities)
+    require("thetto.lib.buffer").open_scratch_tab()
+    vim.bo.filetype = "lua"
+    local lines = vim.split(content, "\n", { plain = true })
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  end,
+
+  action_show_capabilities = function(items)
+    local item = items[1]
+    if not item then
+      return
+    end
+
+    local client = vim.lsp.get_clients({ id = item.client_id })[1]
+    if not item then
+      return
+    end
+
+    local content = vim.inspect(client.capabilities)
+    require("thetto.lib.buffer").open_scratch_tab()
+    vim.bo.filetype = "lua"
+    local lines = vim.split(content, "\n", { plain = true })
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  end,
+}
 
 return M
