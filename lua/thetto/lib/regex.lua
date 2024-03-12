0bfa06aa9f913c437f7eda4e980232a2ed72b815
notomo <notomo.motono@gmail.com>
2024-03-12 20:45:56 +0900

    Fix match_any()

--- lua/thetto/lib/regex.lua
+++ lua/thetto/lib/regex.lua
@@ -1,6 +1,9 @@
 local M = {}
 
 function M.match_any(str, patterns)
+  if not str then
+    return false
+  end
   for _, pattern in ipairs(patterns) do
     local regex = vim.regex(pattern)
     if regex:match_str(str) then
