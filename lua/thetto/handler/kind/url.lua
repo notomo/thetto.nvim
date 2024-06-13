88056c90501068faf2f7870f66ecb2b114072199
notomo <notomo.motono@gmail.com>
2024-06-14 08:04:59 +0900

    Add open_browser action opts

--- lua/thetto/handler/kind/url.lua
+++ lua/thetto/handler/kind/url.lua
@@ -1,13 +1,21 @@
 local M = {}
 
-function M.action_open_browser(items)
+M.opts = {}
+
+M.opts.open_browser = {
+  open = function(url)
+    if not vim.startswith(url, "http") then
+      return "invalid url: " .. url
+    end
+    vim.ui.open(url)
+  end,
+}
+function M.action_open_browser(items, action_ctx)
   local errs = {}
   for _, item in ipairs(items) do
-    local url = item.url
-    if not vim.startswith(url, "http") then
-      table.insert(errs, "invalid url: " .. url)
-    else
-      vim.ui.open(url)
+    local err = action_ctx.opts.open(item.url)
+    if err then
+      table.insert(errs, err)
     end
   end
   if #errs == 0 then
