e898f8da7e6de1493ea2852ded3b46c21120ffe4
notomo <notomo.motono@gmail.com>
2024-08-08 20:49:27 +0900

    Completion on items_changed

--- lua/thetto/handler/consumer/complete/init.lua
+++ lua/thetto/handler/consumer/complete/init.lua
@@ -25,65 +25,68 @@ end
 
 local consumer_events = require("thetto.core.consumer_events")
 
+--- @param self ThettoComplete
+local complete = function(self)
+  local mode = vim.api.nvim_get_mode().mode
+  if mode ~= "i" then
+    return
+  end
+
+  local cursor_word = self._cursor_word or require("thetto.lib.cursor").word(0) or {
+    str = "",
+    offset = 1,
+  }
+  local prefix = cursor_word.str
+
+  local match = function(value)
+    return fn.matchfuzzypos({ value }, prefix)[3][1]
+  end
+  if prefix == "" then
+    match = function()
+      return 0
+    end
+  end
+
+  local scored_items = vim
+    .iter(self._all_items)
+    :map(function(item)
+      local score = match(item.value)
+      if not score then
+        return nil
+      end
+      return {
+        score = score + (self._priorities[item.source_name or ""] or 0),
+        item = item,
+      }
+    end)
+    :totable()
+  table.sort(scored_items, function(a, b)
+    return a.score > b.score
+  end)
+
+  local completion_items = vim
+    .iter(scored_items)
+    :map(function(c)
+      return {
+        word = c.item.value,
+        menu = c.item.kind_label or self._source_to_label[c.item.source_name] or c.item.source_name or c.item.kind_name,
+      }
+    end)
+    :totable()
+  fn.complete(cursor_word.offset, completion_items)
+end
+
+local debounced_complete = require("thetto.vendor.misclib.debounce").wrap(10, vim.schedule_wrap(complete))
+
 local handlers = {
   --- @param self ThettoComplete
   [consumer_events.all.items_changed] = function(self, items, _)
     self._all_items = items
+    debounced_complete(self)
   end,
   --- @param self ThettoComplete
   [consumer_events.all.source_completed] = vim.schedule_wrap(function(self)
-    local mode = vim.api.nvim_get_mode().mode
-    if mode ~= "i" then
-      return
-    end
-
-    local cursor_word = self._cursor_word
-      or require("thetto.lib.cursor").word(0)
-      or {
-        str = "",
-        offset = 1,
-      }
-    local prefix = cursor_word.str
-
-    local match = function(value)
-      return fn.matchfuzzypos({ value }, prefix)[3][1]
-    end
-    if prefix == "" then
-      match = function()
-        return 0
-      end
-    end
-
-    local scored_items = vim
-      .iter(self._all_items)
-      :map(function(item)
-        local score = match(item.value)
-        if not score then
-          return nil
-        end
-        return {
-          score = score + (self._priorities[item.source_name or ""] or 0),
-          item = item,
-        }
-      end)
-      :totable()
-    table.sort(scored_items, function(a, b)
-      return a.score > b.score
-    end)
-
-    local completion_items = vim
-      .iter(scored_items)
-      :map(function(c)
-        return {
-          word = c.item.value,
-          menu = c.item.kind_label
-            or self._source_to_label[c.item.source_name]
-            or c.item.source_name
-            or c.item.kind_name,
-        }
-      end)
-      :totable()
-    fn.complete(cursor_word.offset, completion_items)
+    debounced_complete(self)
   end),
   [consumer_events.all.source_error] = function(_, err)
     vim.notify(require("thetto.vendor.misclib.message").wrap(err), vim.log.levels.WARN)
