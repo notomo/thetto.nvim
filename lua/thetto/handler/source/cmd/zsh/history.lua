0c266ae2ae76c00f4468d747acbfaffcc5b1f867
notomo <notomo.motono@gmail.com>
2025-01-18 20:38:59 +0900

    Async cmd/zsh/history source

--- lua/thetto/handler/source/cmd/zsh/history.lua
+++ lua/thetto/handler/source/cmd/zsh/history.lua
@@ -1,38 +1,47 @@
 local M = {}
 
 function M.collect(source_ctx)
-  local path = vim.fn.systemlist({ "zsh", "-i", "-c", "echo ${HISTFILE}" })[1]
-  if not path then
-    return {}
+  return function(observer)
+    vim.system(
+      { "zsh", "-i", "-c", "echo ${HISTFILE}" },
+      {
+        cwd = source_ctx.cwd,
+      },
+      vim.schedule_wrap(function(o)
+        local path = vim.trim(o.stdout)
+        if not path then
+          observer:next({})
+          observer:complete()
+        end
+
+        local f = io.open(path, "r")
+        if not f then
+          observer:error("cannot open: " .. path)
+          return
+        end
+        local lines = vim.fn.reverse(vim.split(f:read("*a"), "\n", { plain = true }))
+        f:close()
+
+        local items = vim
+          .iter(lines)
+          :map(function(s)
+            local cmd = s:gsub(".*;", "")
+            if cmd == "" then
+              return nil
+            end
+            return {
+              value = cmd,
+              cwd = source_ctx.cwd,
+              shell = "zsh",
+            }
+          end)
+          :totable()
+
+        observer:next(items)
+        observer:complete()
+      end)
+    )
   end
-
-  local f = io.open(path, "r")
-  if not f then
-    return {}
-  end
-  local lines = vim.fn.reverse(vim.split(f:read("*a"), "\n", { plain = true }))
-  f:close()
-
-  local cmds = vim
-    .iter(lines)
-    :map(function(s)
-      local x = s:gsub(".*;", "")
-      return x
-    end)
-    :totable()
-  cmds = vim
-    .iter(cmds)
-    :filter(function(cmd)
-      return cmd ~= ""
-    end)
-    :totable()
-
-  return vim
-    .iter(cmds)
-    :map(function(cmd)
-      return { value = cmd, cwd = source_ctx.cwd, shell = "zsh" }
-    end)
-    :totable()
 end
 
 M.kind_name = "cmd/shell/cmd"
